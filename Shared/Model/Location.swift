f7ab81a9677792fbf11a36aba151a56ed0a23df5
yining wang <ywang@lcs.on.ca>
2022-01-18 09:45:35 -0500

    adding structure of location

---
+++ Shared/Model/Location.swift
@@ -0,0 +1,23 @@
+//
+//  Location.swift
+//  TrckerMultiPlatform
+//
+//  Created by Yining Wang on 2022-01-18.
+//
+
+import Foundation
+
+struct Location {
+    let id: Int
+    let name: String
+    let country: String
+    let description: String
+    let more: String
+    let latitude: Double
+    let longitude: Double
+    let heroPicture: String
+    let advisory: String
+    
+    static let example = Location(id: 1, name: <#T##String#>, country: <#T##String#>, description: <#T##String#>, more: <#T##String#>, latitude: <#T##Double#>, longitude: <#T##Double#>, heroPicture: <#T##String#>, advisory: <#T##String#>)
+    
+}
