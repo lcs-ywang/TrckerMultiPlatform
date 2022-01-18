f3738bc01fb30415e6da07c711b0d4b56008b883
yining wang <ywang@lcs.on.ca>
2022-01-18 09:23:00 -0500

    Initial Commit

---
+++ Shared/TrckerMultiPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrckerMultiPlatformApp.swift
+//  Shared
+//
+//  Created by Yining Wang on 2022-01-18.
+//
+
+import SwiftUI
+
+@main
+struct TrckerMultiPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
