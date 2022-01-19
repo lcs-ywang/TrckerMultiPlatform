567fba837f044de8bad5b0d7e0e5f61b383d38db
yining wang <ywang@lcs.on.ca>
2022-01-19 10:21:30 -0500

    Added location store to load locations from JSON file

---
+++ Shared/Model/LocationStore.swift
@@ -0,0 +1,28 @@
+//
+//  LocationStore.swift
+//  TrckerMultiPlatform
+//
+//  Created by Yining Wang on 2022-01-19.
+//
+
+import Foundation
+
+class LocationStore: ObservableObject{
+   
+    var places: [Location]
+   
+    init() {
+        
+        let url = Bundle.main.url(forResource: "locations", withExtension: "json")!
+        
+        let data = try! Data(contentsOf: url)
+        
+        places = try! JSONDecoder().decode([Location].self, from: data)
+        
+        places.sort(by: { $0.name < $1.name
+        
+        })
+                      
+    }
+    
+}
