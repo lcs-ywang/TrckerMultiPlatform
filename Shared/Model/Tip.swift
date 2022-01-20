73b3acba2f2b0a9bd3a3591778d59620c9b5b21f
yining wang <ywang@lcs.on.ca>
2022-01-19 22:37:17 -0500

    Added tip model

---
+++ Shared/Model/Tip.swift
@@ -0,0 +1,20 @@
+//
+//  Tip.swift
+//  TrckerMultiPlatform
+//
+//  Created by Yining Wang on 2022-01-19.
+//
+
+import SwiftUI
+
+struct Tip: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Tip_Previews: PreviewProvider {
+    static var previews: some View {
+        Tip()
+    }
+}
